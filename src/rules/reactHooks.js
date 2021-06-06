c9951fdbc0af6237efdec21c302edfffcb9d46cb
Tobias Reich <tobias.reich.ich@gmail.com>
2021-06-06 10:46:39 +0200

    Split into files

---
+++ src/rules/reactHooks.js
@@ -0,0 +1,4 @@
+module.exports = {
+	'react-hooks/rules-of-hooks': 2,
+	'react-hooks/exhaustive-deps': 0,
+}
---
+++ src/rules/unicorn.js
@@ -0,0 +1,53 @@
+module.exports = {
+	'unicorn/no-useless-undefined': 2,
+	'unicorn/no-unused-properties': 2,
+	'unicorn/consistent-destructuring': 2,
+	'unicorn/error-message': 2,
+	'unicorn/new-for-builtins': 2,
+	'unicorn/prefer-spread': 2,
+	'unicorn/prefer-includes': 2,
+	'unicorn/prefer-date-now': 2,
+	'unicorn/prefer-array-flat': 2,
+	'unicorn/prefer-default-parameters': 2,
+	'unicorn/prefer-switch': 2,
+	'unicorn/numeric-separators-style': 2,
+	'unicorn/require-array-join-separator': 2,
+	'unicorn/require-number-to-fixed-digits-argument': 2,
+	'unicorn/prefer-prototype-methods': 2,
+	'unicorn/prefer-object-has-own': 2,
+	'unicorn/filename-case': [ 2, {
+		cases: {
+			camelCase: true,
+			pascalCase: true,
+		},
+	}],
+	'unicorn/prevent-abbreviations': [ 2, {
+		extendDefaultReplacements: false,
+		replacements: {
+			conf: {
+				config: true,
+			},
+			i: {
+				index: true,
+			},
+			err: {
+				error: true,
+			},
+			ev: {
+				event: true,
+			},
+			evt: {
+				event: true,
+			},
+			opt: {
+				option: true,
+			},
+			opts: {
+				options: true,
+			},
+			val: {
+				value: true,
+			},
+		},
+	}],
+}
