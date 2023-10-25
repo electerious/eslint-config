6ba45663b1fe4cfb9baa9ff9540c77920635c159
Tobias Reich <tobias.reich.ich@gmail.com>
2023-10-25 16:14:51 +0200

    Format with prettier

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 module.exports = {
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-		ecmaFeatures: {
-			jsx: true,
-		},
-	},
-	plugins: [ 'ava', 'import', 'react', 'react-hooks', 'unicorn' ],
-	extends: [ 'plugin:ava/recommended' ],
-	env: {
-		browser: true,
-		commonjs: true,
-		es6: true,
-		mocha: true,
-		node: true,
-	},
-	rules: {
-		...require('./rules/eslint'),
-		...require('./rules/import'),
-		...require('./rules/react'),
-		...require('./rules/reactHooks'),
-		...require('./rules/unicorn'),
-	},
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['ava', 'import', 'react', 'react-hooks', 'unicorn'],
+  extends: ['plugin:ava/recommended'],
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    mocha: true,
+    node: true,
+  },
+  rules: {
+    ...require('./rules/eslint'),
+    ...require('./rules/import'),
+    ...require('./rules/react'),
+    ...require('./rules/reactHooks'),
+    ...require('./rules/unicorn'),
+  },
 }
