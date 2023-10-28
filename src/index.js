e9e3a99db6e40d3070677c35989245f62730cefd
Tobias Reich <tobias.reich.ich@gmail.com>
2023-10-28 10:38:51 +0200

    Remove react rules that affect the styling and set es2024 env

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 module.exports = {
   parserOptions: {
-    ecmaVersion: 'latest',
     sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
@@ -11,9 +10,9 @@ module.exports = {
   env: {
     browser: true,
     commonjs: true,
-    es6: true,
     mocha: true,
     node: true,
+    es2024: true,
   },
   rules: {
     ...require('./rules/eslint.js'),
--- src/rules/react.js
+++ src/rules/react.js
@@ -1,6 +1,4 @@
 module.exports = {
-  'react/jsx-closing-bracket-location': 2,
-  'react/jsx-closing-tag-location': 2,
   'react/jsx-curly-brace-presence': [
     2,
     {
@@ -8,15 +6,6 @@ module.exports = {
       props: 'never',
     },
   ],
-  'react/jsx-curly-spacing': [
-    2,
-    {
-      children: true,
-      when: 'always',
-    },
-  ],
-  'react/jsx-equals-spacing': [2, 'never'],
-  'react/jsx-first-prop-new-line': 2,
   'react/jsx-no-comment-textnodes': 2,
   'react/jsx-no-duplicate-props': 2,
   'react/jsx-no-target-blank': 2,
@@ -26,6 +15,5 @@ module.exports = {
       allowAllCaps: true,
     },
   ],
-  'react/jsx-tag-spacing': 2,
   'react/jsx-uses-vars': 2,
 }
